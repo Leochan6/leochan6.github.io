57e05270611f91947340ddc96ea7412002a942c2
Leochan6 <leo.chan6@yahoo.ca>
2020-03-03 11:53:21 -0500

    Added Form Checking

--- script/character.js
+++ script/character.js
@@ -1,17 +1,38 @@
-let character = (() => {
+let character_api = (() => {
 
   let module = {};
 
-  module.collection = {};
+  module.Display = class Display {
+    constructor(id, name, rank, attr, level, magic, magia, episode) {
+      this.id = id;
+      this.name = name;
+      this.rank = rank;
+      this.attr = attr;
+      this.level = level;
+      this.magic = magic;
+      this.magia = magia;
+      this.episode = episode;
+    }
+  }
 
+  module.Character = class Character {
+    constructor(id, name, attr, ranks) {
+      this.id = id;
+      this.name = name;
+      this.attr = attr;
+      this.ranks = ranks;
+    }
+  }
+  
   // sends HTML request to get the collection json.
 	module.getCollection = (callback) => {
 		var xhr = new XMLHttpRequest();
 		xhr.onload = () => callback(JSON.parse(xhr.responseText));
     xhr.open("GET", "/assets/magireco/collection.json", true);
-	  xhr.send();
+    xhr.send();
   };
 
+  // get the list of names.
   module.getNames = (callback) => {
     module.getCollection((collection) => {
       let names = collection.map(character => {return {id: character.id, name: character.name}});
@@ -20,33 +41,70 @@ let character = (() => {
     })
   };
 
-  module.getAttributeRank = (id, callback) => {
+  // get the attribute and rank for the character.
+  module.getCharacter = (id, callback) => {
     module.getCollection((collection) => {
-      let attribute = collection.filter((character) => character.id === id)[0].attribute.toLowerCase();
-      let ranks = collection.filter((character) => character.id === id).map((character) => character.rank);
-      let hasRanks = [false, false, false, false, false];
+      let character_list = collection.filter(character => character.id === id);
+      let name = character_list[0].name
+      let attribute = character_list[0].attribute.toLowerCase();
+      let rank_list = character_list.map((character) => character.rank);
+      let ranks = Array(5).fill(false);
       for (let i = 0; i < 5; i++) {
-        if (ranks.indexOf((i+1).toString(10)) != -1) hasRanks[i] = true;
+        if (rank_list.indexOf((i+1).toString(10)) != -1) ranks[i] = true;
       }
-      callback(attribute, hasRanks);
+      let character = new module.Character(id, name, attribute, ranks);
+      callback(character);
     });
   };
-  
-  module.showDisplay = ({id, rank, attr, level, magic, magia, episode}) => {
-    console.log("show", id, rank, attr, level, magic, magia, episode);
-    let display_div = document.querySelector("#display");
-    display_div.innerHTML = `
-    <div class="character_display" character_id="${id}">
-      <img class="background" src="assets/magireco/ui/bg/${attr}.png">
-      <img class="card_image" src="assets/magireco/card/image/card_${id}${rank}_d.png">
-      <img class="frame_rank" src="assets/magireco/ui/frame/${rank}.png">
-      <img class="star_rank" src="assets/magireco/ui/star/${rank}.png">
-      <img class="att" src="assets/magireco/ui/attr/${attr}.png">
-      <img class="magic" src="assets/magireco/ui/magic/${magic}.png">
-      <img class="magia" src="assets/magireco/ui/magia/${magia}-${episode}.png">
-      <div class="level">${level}</div>
-    </div>`
+
+  module.getCharacterDisplay = (character) => {
+    return new module.Display(character.id, character.name, character.ranks.indexOf(true) + 1, character.attr, "1", "0", "1", "1");
   };
+
+  // check if disaply is valid.
+  module.isValidCharacterDisplay = (display, character) => {
+    let err = [];
+    // check id.
+    if (display.id !== character.id) err.push(`Display Id ${display.id} does not match Character ID ${character.id}.`);
+    // check name.
+    if (display.name !== character.name) err.push(`Display Name ${display.name} does not match Character Name ${character.Name}.`);
+    // check rank.
+    if (!character.ranks[display.rank - 1]) err.push(`Display Rank ${display.rank} does not match Character Ranks ${character.ranks}`)
+    // check level.
+    if      (display.rank == "1" && (display.level < 1 || display.level > 40)) err.push(`Display Level ${display.level} for Display Rank ${display.rank} must be between 1 and 40.`);
+    else if (display.rank == "2" && (display.level < 1 || display.level > 50)) err.push(`Display Level ${display.level} for Display Rank ${display.rank} must be between 1 and 50.`);
+    else if (display.rank == "3" && (display.level < 1 || display.level > 60)) err.push(`Display Level ${display.level} for Display Rank ${display.rank} must be between 1 and 60.`);
+    else if (display.rank == "4" && (display.level < 1 || display.level > 80)) err.push(`Display Level ${display.level} for Display Rank ${display.rank} must be between 1 and 80.`);
+    else if (display.rank == "5" && (display.level < 1 || display.level > 100)) err.push(`Display Level ${display.level} for Display Rank ${display.rank} must be between 1 and 100.`);
+    // check magic.
+    if (display.magic < 0 || display.magic > 3) err.push(`Display Magic ${display.magic} must be between 0 and 3.`);
+    // check magia.
+    if (display.magia < 1 || display.magia > 5) err.push(`Display Magia ${display.magia} must be between 1 and 5.`);
+    if (display.magia > display.episode) err.push(`Display Magia ${display.magia} must be less than or equal to Display Episode ${display.episode}.`);
+    // check episode.
+    if (display.episode < 1 || display.episode > 5) err.push(`Display Episode ${display.episode} must be between 1 and 5.`);
+
+    return err;
+  };
+
+  module.updateDisplay = (character, display) => {
+    // return the default display.
+    if (!display) return module.getCharacterDisplay(character);
+    return new module.Display(character.id, character.name, display.rank, character.attr, display.level, display.magic, display.magia, display.episode);
+  }
+
+  let displayListeners = [];
+
+  module.onDisplayUpdate = (listener) => {
+    displayListeners.push(listener);
+    module.getCharacter("1001", character => {
+      listener(character, module.getCharacterDisplay(character));
+    });
+  }
+
+  // let notifyDisplayListeners = (display) => {
+  //   displayListeners.forEach(listener => listener(display));
+  // }
 
   return module;
 })();
--- script/index.js
+++ script/index.js
@@ -3,47 +3,104 @@
 
   // const collection = {ids: [1001, 1002, 1003]};
 
-  class Display {
-    constructor(id, rank, attr, level, magic, magia, episode) {
-      this.id = id;
-      this.rank = rank;
-      this.attr = attr;
-      this.level = level;
-      this.magic = magic;
-      this.magia = magia;
-      this.episode = episode;
+  const name_select = document.querySelector("#name_select");
+  const rank_select = document.querySelector("#rank_select");
+  const attr_select = document.querySelector("#attr_select");
+  const level_select = document.querySelector("#level_select");
+  const magic_select = document.querySelector("#magic_select");
+  const magia_select = document.querySelector("#magia_select");
+  const episode_select = document.querySelector("#episode_select");
+
+  const display_preview = document.querySelector("#display_preview");
+
+  const getDisplay = () => {
+    let display = new character_api.Display(
+      name_select.value,
+      name_select[name_select.options.selectedIndex].text,
+      rank_select.value,
+      attr_select.value,
+      level_select.value,
+      magic_select.value,
+      magia_select.value,
+      episode_select.value);
+    character_preview = display;
+    return display;
+  };
+
+  const updateDisplay = (display) => {
+    console.log("update display", display);
+    display_preview.innerHTML = `
+    <div class="character_display" character_id="${display.id}" character_name="${display.name}">
+      <img class="background" src="assets/magireco/ui/bg/${display.attr}.png">
+      <img class="card_image" src="assets/magireco/card/image/card_${display.id}${display.rank}_d.png">
+      <img class="frame_rank" src="assets/magireco/ui/frame/${display.rank}.png">
+      <img class="star_rank" src="assets/magireco/ui/star/${display.rank}.png">
+      <img class="att" src="assets/magireco/ui/attr/${display.attr}.png">
+      <img class="magic" src="assets/magireco/ui/magic/${display.magic}.png">
+      <img class="magia" src="assets/magireco/ui/magia/${display.magia}-${display.episode}.png">
+      <div class="level">${display.level}</div>
+    </div>`
+  };
+
+  const updateForm = (display) => {
+    console.log("update form", display);
+    name_select.value = display.id;
+    rank_select.value = display.rank;
+    attr_select.value = display.attr;
+    level_select.value = display.level;
+    magic_select.value = display.magic;
+    magia_select.value = display.magia;
+    episode_select.value = display.episode;
+  };
+
+  const updateFormEnabled = (character) => {
+    console.log("disabling", character);
+    // enable or disable the attribute select.
+    for (let i = 0; i < 6; i++) {
+      attr_select.options[i].disabled = attr_select.options[i].value != character.attr;
     }
-  }
+    attr_select.value = character.attr;
+    // enable or disable the rank select.
+    for (let i = 0; i < 5; i++) {
+      rank_select.options[i].disabled = !character.ranks[i];
+    }
+    // if the currently select rank is disabled, then select minimum available rank.
+    if (!character.ranks[rank_select.selectedIndex]) {
+      rank_select.selectedIndex = character.ranks.indexOf(true);
+    }
+  };
+
+  let character_preview = null;
 
   window.onload = () => {
     // update attribute and rank fields on name change.
-    document.querySelector("#id_select").addEventListener("change", () => {
-      character.getAttributeRank(document.querySelector("#id_select").value, (attribute, ranks) => {
-        document.querySelector("#attr_select").value = attribute;
-        // disable if rank not available and set value equal to lowest rank.
-        for (var i = 4; i >= 0; i--) {
-          document.querySelector("#rank_select").options[i].disabled = !ranks[i];
-          if (ranks[i]) {
-            document.querySelector("#rank_select").value = (i+1).toString(10);
-          }
-        }
-        // disable if not attribute
-        for (var i = 0; i < 6; i++) {
-          document.querySelector("#attr_select").options[i].disabled = document.querySelector("#attr_select").options[i].value != attribute;
-        }
-        getAndShow();
+    name_select.addEventListener("change", () => {
+      character_api.getCharacter(name_select.value, character => {
+        updateFormEnabled(character);
+        character_preview = character_api.updateDisplay(character, getDisplay());
+        updateForm(character_preview);
+        updateDisplay(character_preview);
       });
     });
-    document.querySelector("#rank_select").addEventListener("change", getAndShow);
-    document.querySelector("#attr_select").addEventListener("change", getAndShow);
-    document.querySelector("#level_select").addEventListener("change", getAndShow);
-    document.querySelector("#magic_select").addEventListener("change", getAndShow);
-    document.querySelector("#magia_select").addEventListener("change", getAndShow);
-    document.querySelector("#episode_select").addEventListener("change", getAndShow);
+
+    document.querySelectorAll(".form").forEach(element => {
+      element.addEventListener("change", () => {
+        character_api.getCharacter(name_select.value, character => {
+          let display = getDisplay();
+          let err = character_api.isValidCharacterDisplay(display, character);
+          if (err.length == 0) {
+            updateDisplay(display);
+          }
+          else {
+            console.log(err);
+          }
+        });
+      });
+    });
   };
 
   // initilize name field.
-  character.getNames((collection) => {
+  character_api.getNames((collection) => {
     let id_select = document.querySelector("#id_select");
     let prev_id = null;
     let characters = [];
@@ -54,23 +111,19 @@
       }
     })
     characters.forEach((character) => {
-      id_select.options.add(new Option(character.name, character.id, false));
+      name_select.options.add(new Option(character.name, character.id, false));
     });
-    document.querySelector("#id_select").value = "1001";
-    document.querySelector("#id_select").dispatchEvent(new Event("change"));
+    name_select.selectedIndex = 0;
+    name_select.dispatchEvent(new Event("change"));
   });
 
-  let getAndShow = () => {
-    let display = new Display(
-      document.querySelector("#id_select").value,
-      document.querySelector("#rank_select").value,
-      document.querySelector("#attr_select").value,
-      document.querySelector("#level_select").value,
-      document.querySelector("#magic_select").value,
-      document.querySelector("#magia_select").value,
-      document.querySelector("#episode_select").value);
-    character.showDisplay(display);
-  }
+  character_api.onDisplayUpdate((character, display) => {
+    updateFormEnabled(character);
+    // updateForm(display);
+    updateDisplay(display);
+  });
+
+  
 
 
 
